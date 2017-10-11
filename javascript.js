27354a8dfd65fdc4d688916eba4118c86886199f
Nora Gully <nora.gully@gmail.com>
2017-10-11 17:29:10 -0600

    Made game work range is in progress

--- javascript.js
+++ javascript.js
@@ -1,38 +1,58 @@
-//   V A R I A B L E S
+//   TURING PROJECT :: N U M B E R  G U E S S E R :: BY GULLY
+//   Assignment URL: http://frontend.turing.io/projects/number-guesser.html
+
+
+
+//   V A R I A B L E S 
 var guessBtn = document.getElementById('guessButton'); //this is just grabbing the button
-var clearButton = document.getElementById("clearButton");
-var resetButton = document.getElementById("resetButton");
-var guessInput = document.getElementById("guessInputText"); //this is grabbing the string from the input field
-var userMin = document.getElementById("minInput");
-var userMax = document.getElementById("maxInput"); //this var is just the max input
+var clearButton = document.getElementById("clearButton"); // clear button
+var resetButton = document.getElementById("resetButton"); // reset button
+var guessInput = document.getElementById("guessInputText"); // grabbing the string from the input field
+var userMin = document.getElementById("minInput"); // min input string
+var userMax = document.getElementById("maxInput"); // max input string
+var genNumber = document.getElementById('minMaxButton'); // number generator button
+var realMin = 0; // default value for Min - gets reassigned in genNumber
+var realMax = 100; // default value for Max - gets reassigned in genNumber
+var correctNumber = getRandomArbitrary(0, 100);
 
-// I only want to generate this if there is a min and max present
 
-var correctNumber = getRandomArbitrary(userMinNumber, userMaxNumber);
-var userMinNumber = getMinNumber(userMin);
-var userMaxNumber = getMaxNumber(userMax);
 
 //   E V E N T   L I S T E N E R S
-guessBtn.addEventListener('click', guessInputSubmit);
+guessBtn.addEventListener('click', function(){
+  //realMin = getMinNumber(userMin); 
+  //realMax = getMaxNumber(userMax);
+  //minMaxCheck(realMin, realMax);
+  guessInputSubmit();
+});
 clearButton.addEventListener('click', clearInputSubmit);
 resetButton.addEventListener('click', resetButtonSubmit);
 guessInput.addEventListener('keyup', enableButtons);
-userMax.addEventListener('keyup', getMaxNumber);
-userMin.addEventListener('keyup', getMinNumber); // this is running function getMinNumber upon keyup
+userMin.addEventListener('keyup', function(){
+  getMinNumber(userMin);
+});
+userMax.addEventListener('keyup',function(){
+  getMaxNumber(userMax);
+});
+genNumber.addEventListener('click', function(){
+  realMin = getMinNumber(userMin); 
+  realMax = getMaxNumber(userMax);
+  correctNumber = getRandomArbitrary(realMin, realMax);
+});
 
-
-//   make min and max strings into numbers
-function getMinNumber(){
-  var minInt = parseInt(userMin.value);
+//   FUNCTIONS THAT make min and max strings into numbers, called up there ^^
+function getMinNumber(string){
+  var minInt = parseInt(string.value);
   console.log("Your min is " + minInt);
+  document.querySelector('#printMin').textContent = string.value;
   return minInt;
 }
-function getMaxNumber(){
-  var maxInt = parseInt(userMax.value);
+
+function getMaxNumber(string){
+  var maxInt = parseInt(string.value);
   console.log("Your max is " + maxInt);
+  document.querySelector('#printMax').textContent = string.value;
   return maxInt;
 }
-
 
 //   ENABLE THE BUTTONS WHEN TEXT IS ENTERED
 function enableButtons(){
@@ -41,7 +61,6 @@ function enableButtons(){
   document.getElementById('resetButton').disabled = false;
 }
 
-
 //   GENERATE A RANDOM WHOLE NUMBER
 function getRandomArbitrary(min, max) {
   var randomNumber = Math.round(Math.random() * (max - min) + min);
@@ -49,17 +68,9 @@ function getRandomArbitrary(min, max) {
   return randomNumber;
 }
 
-//   WHAT HAPPENS WHEN YOU HIT THE 'GUESS' BUTTON   \\
-function guessInputSubmit(event){
-  event.preventDefault();    
-  var guessedNumber = parseInt(guessInput.value);
-  console.log("You guessed " + guessedNumber);
-  console.log("Which is a " + typeof guessedNumber);
-
-
-  //   CHECK THE GUESS IS BETWEEN MIN AND MAX VALUES   \\
-  function minMaxCheck (min, max) {
-    if (guessedNumber < min )  {  //&& isNaN(parseInt(value)))
+//   CHECK THAT THE GUESS IS IN RANGE 
+function minMaxCheck(min, max) {
+    if (guessedNumber < min )  {  
         alert("Please enter a number greater than " + min); 
     }
     else if (guessedNumber > max) {
@@ -70,26 +81,29 @@ function guessInputSubmit(event){
     }
     else 
         console.log("wtf");
-   }  
-  minMaxCheck(userMinNumber, userMaxNumber);
+   }
 
-
-  //   COMPARING THE USER AND RANDOM NUMBER   \\
-    if (guessedNumber > correctNumber) {
-     document.querySelector('#clueText').innerText = "That's too high!";
-    }
-    else if (guessedNumber < correctNumber) {
-      document.querySelector('#clueText').innerText = "That's too low!";
-    }
-    else if (guessedNumber === correctNumber){
-      document.querySelector('#clueText').innerText = "FUCK YEAH!";
-      var unicornCorral = document.getElementById('corral');
-      unicornCorral.insertAdjacentHTML('afterbegin', '<img src="unicorn.gif" width="200px" margin="auto">');
-    }
-    else 
-      alert("Please enter a number.");
-    
+//   WHAT HAPPENS WHEN YOU HIT THE 'GUESS' BUTTON   \\
+function guessInputSubmit(){
+  event.preventDefault();    
+  var guessedNumber = parseInt(guessInput.value);
+  console.log("You guessed " + guessedNumber);
+  console.log("Which is a " + typeof guessedNumber);  
   document.querySelector('#guessText').innerText = guessedNumber;
+//   COMPARING THE USER NUMBER AND RANDOMLY GENERATED NUMBER   \\
+  if (guessedNumber > correctNumber) {
+   document.querySelector('#clueText').innerText = "That's too high!";
+  }
+  else if (guessedNumber < correctNumber) {
+    document.querySelector('#clueText').innerText = "That's too low!";
+  }
+  else if (guessedNumber === correctNumber){
+    document.querySelector('#clueText').innerText = "HELL YEAH!";
+    var unicornCorral = document.getElementById('corral');
+    unicornCorral.insertAdjacentHTML('afterbegin', '<img src="unicorn.gif" width="200px" margin="auto">');
+  }
+  else 
+    alert("Please enter a number.");
 }
 
 
@@ -109,8 +123,13 @@ function resetButtonSubmit(event) {
   document.querySelector('#clueText').textContent = " ";
   document.querySelector('#guessText').textContent = " ";
   document.getElementById("resetButton").disabled = true;
-
 }
 
+//  |  |  |  |  C O D E   J A I L  |  |  |  |  
+//userMax.addEventListener('keyup', getMaxNumber);
+//userMin.addEventListener('keyup', getMinNumber); // this is running function getMinNumber upon keyup
+//var userMinNumber = getMinNumber(userMin);
+//var userMaxNumber = getMaxNumber(userMax);
+//
 //   HERE IS THE BOTTOM OF THE DOCUMENT!!!   \\
 //          THANK YOU FOR PLAYING!!!         \\
